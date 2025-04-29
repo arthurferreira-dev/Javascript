700ce6a9b04b2cad6e8a904f228bcace8cf89f0f
Arthur Ferreira <tutudacosta996@gmail.com>
2025-04-29 14:56:17 -0300

    finish teste.js aula 13

--- Módulo E/aula13/teste/teste.js
+++ Módulo E/aula13/teste/teste.js
@@ -1,5 +1,11 @@
 var number = 1
-while (number < 12) {
+while (number <= 12) {
     console.log(`Metros ${number}`)
     number++
 }
+
+var day = 1
+do {
+    console.log(`${day} Dias passados`)
+    day++
+} while (day <= 31)
