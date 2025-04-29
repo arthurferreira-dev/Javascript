94aa6a8503a9fce8e0fa0d4f91a8bd3597a051b4
Arthur Ferreira <tutudacosta996@gmail.com>
2025-04-29 14:54:25 -0300

    finish aula 13

---
+++ Módulo E/aula13/ambiente.js
@@ -0,0 +1,22 @@
+//console.log('Repetição em JS')
+
+var cont = 1
+do { // Mesma funcionalidade (Teste lógico no final)
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 6)
+
+    var cont = 1
+    while (cont <= 6) { // Mesma funcionalidade (Teste lógico no inicio)
+    console.log(`Andar ${cont}`)
+    cont++
+    }
+
+/* Mesma funcionalidade só que pior
+console.log('aaa')
+console.log('aaa')
+console.log('aaa')
+console.log('aaa')
+console.log('aaa')
+console.log('aaa')
+*/
---
+++ Módulo E/aula13/teste/teste.js
@@ -0,0 +1,5 @@
+var number = 1
+while (number < 12) {
+    console.log(`Metros ${number}`)
+    number++
+}
