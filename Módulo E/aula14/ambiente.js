8f83d42c24275cb3652537b61bc596841a3d9dba
Arthur Ferreira <tutudacosta996@gmail.com>
2025-04-29 15:17:30 -0300

    finish aula 14

---
+++ Módulo E/aula14/ambiente.js
@@ -0,0 +1,20 @@
+/*/
+var c = 1
+do {
+    console.log(`${c} passos percorridos`)
+    c++
+} while (c <= 6)
+*/
+    //----------
+    console.log('Vai começar...')
+    var f = 10
+    var one = 1
+    var ten = 10
+
+    for(var c = one; c <= f; c++) {
+        console.log(c)
+        if (c == ten) {
+            console.log('Quase e...')
+        }
+    }
+    console.log('FIM!')
