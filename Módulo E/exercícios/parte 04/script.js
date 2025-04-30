88b2d5e317ee4102ad842445037323520d84c1f0
Arthur Ferreira <tutudacosta996@gmail.com>
2025-04-30 18:08:44 -0300

    finish ex part 04

--- Módulo E/exercícios/parte 04/script.js
+++ Módulo E/exercícios/parte 04/script.js
@@ -4,6 +4,31 @@ function contador() {
     let passo = document.getElementById('txtpas')
     let res = document.querySelector('div#res')
 
-    if (Number(inicio.value.length) == 0 || Number(inicio.value.length) == 0)
+    if (inicio.value.length == 0 ||  fim.value.length == 0 || passo.value.length == 0) {
+        //alert('[ERROR] Faltam Dados [ERROR]')
+        res.innerHTML = `[ERROR] Faltam Dados [ERROR]`
+    } else {
+        res.innerHTML = `Contando... <br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
+        if (p <= 0) {
+            alert('Passo inválido, passo irá valer 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // Crescente
+            for (let c = i ; c <= f ; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }   
+        } else {
+            // Descrecente
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
