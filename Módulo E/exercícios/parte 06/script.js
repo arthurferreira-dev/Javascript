304bab14f1fd1805616387c85c89553ea96c5b52
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-02 15:25:00 -0300

    while to for

--- Módulo E/exercícios/parte 06/script.js
+++ Módulo E/exercícios/parte 06/script.js
@@ -6,14 +6,12 @@ function Gerador() {
         alert('Por favor, digite algum número!')
     } else {
         let n = Number(num.value)
-        let c = 1
         tab.innerHTML = ''
-        while (c <= 10) {
+        for (let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
-            c++
         }
     }
 }
