fc91c32568acfaf7e7515629426032b3e7222a0b
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-02 19:28:05 -0300

    penúltima aula

--- Módulo F/exercícios/parte 07 e 08/script.js
+++ Módulo F/exercícios/parte 07 e 08/script.js
@@ -21,8 +21,58 @@ function inLista(n, l) {
 
 function Add() {
     if (isNumero(num.value) && !inLista(num.value, vetor)) {
-        
+        vetor.push(Number(num.value))
+
+        let n = Number(num.value)
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado`
+        item.value = `val${n}`
+        tab.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrada')
+    }
+    num.value = '' // Zera o input number
+    num.focus() // Deixa no input number
+}
+
+function Final() {
+    if (vetor.length == 0) {
+        alert('Adicione valores antes de valores!')
+    } else {
+        let total = vetor.length
+        let big = vetor[0]
+        let small = vetor[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in vetor) { // vai analisar um por um
+            soma += vetor[pos]
+            if (vetor[pos] > big) {
+                big = vetor[pos]
+            }
+            if (vetor[pos] < small) {
+                small = vetor[pos]
+            }
+        }
+
+        media = soma / total
+
+        res.innerHTML = ''
+
+        res.innerHTML += '<h2>Total:</h2>' //título bonitin ^w^
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+
+        res.innerHTML += '<h2>Maior Número:</h2>'
+        res.innerHTML += `<p>O maior valor informado foi ${big}.</p>`
+    
+        res.innerHTML += '<h2>Menor Número:</h2>'
+        res.innerHTML += `<p>O menor valor informado foi ${small}.</p>`
+
+        res.innerHTML += '<h2>Soma:</h2>'
+        res.innerHTML += `<p>A soma de todos os valores dá ${soma}.</p>`
+
+        res.innerHTML += '<h2>Média:</h2>'
+        res.innerHTML += `<p>A média dos valores é ${media}.</p>`
     }
 }
