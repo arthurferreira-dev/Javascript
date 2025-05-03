4f492faf9fca49a701b5e17f3634323664eb48a0
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-02 23:18:52 -0300

    prática js (object)

---
+++ Módulo F/aula17 (última aula)/práticas/pr001.js
@@ -0,0 +1,5 @@
+let miguel = {nome: 'Miguel', idade: 12, sexo: 'Masculino'}
+
+console.log(`Seu nome é ${miguel.nome}`)
+console.log(`Sua idade é ${miguel.idade}`)
+console.log(`Seu sexo é ${miguel.sexo}`)
---
+++ Módulo F/aula17 (última aula)/práticas/pr002.js
@@ -0,0 +1,6 @@
+let andre = {nome: 'André', idade: 12, sexo: 'Masculino', nascimento_dia: 19, nascimento_mês: 'Dezembro', nascimento_ano: 2012}
+
+console.log(`Seu nome é ${andre.nome}`)
+console.log(`Sua idade é ${andre.idade} anos`)
+console.log(`Seu sexo é ${andre.sexo}`)
+console.log(`${andre.nome} nasceu no dia ${andre.nascimento_dia} no mês de ${andre.nascimento_mês} no ano de ${andre.nascimento_ano}`)
